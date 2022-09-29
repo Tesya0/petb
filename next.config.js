f0a2e87a15f568f06974b0418cf99dbab38249e2
sasaki <sasaki@MacBook-Pro-M1-Pro.local>
2022-09-29 17:25:28 +0900

    Security Headersを設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const headers = require('./headers');
+
 const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
@@ -6,6 +8,15 @@ const nextConfig = {
   i18n,
   images: {
     domains: ['images.dog.ceo'],
+  },
+  async headers() {
+    return [
+      {
+        // 全てのパスに Security Headers を適用する
+        source: '/(.*)',
+        headers,
+      },
+    ];
   },
 };
 
